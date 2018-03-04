389880aa97827fbec8db7c09d411acc92ce912dc
Damen King <damen@terrastride.com>
2018-03-03 19:44:31 -0800

    django rest endpoint for happyhours

--- django-app/src/api/api.py
+++ django-app/src/api/api.py
@@ -1,7 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Business
-from .serializers import BusinessSerializer
+from .models import Business, Happyhour
+from .serializers import BusinessSerializer, HappyhourSerializer
 
 class BusinessViewSet(ModelViewSet):
     queryset = Business.objects.all()
     serializer_class = BusinessSerializer
+
+class HappyhourViewSet(ModelViewSet):
+    queryset = Happyhour.objects.all()
+    serializer_class = HappyhourSerializer
--- django-app/src/api/serializers.py
+++ django-app/src/api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from .models import Business
+from .models import Business, Happyhour
 
 
 class BusinessSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Business
+        fields = '__all__'
+
+
+class HappyhourSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Happyhour
         fields = '__all__'
